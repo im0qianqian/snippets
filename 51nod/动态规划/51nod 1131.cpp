d7218f0531fa9b218d395a0877de561dd42cf07e
千千 <qian1335661317@qq.com>
2016-08-05 14:42:52 +0800

    2016.08.05

---
+++ 51nod/动态规划/51nod 1131.cpp
@@ -0,0 +1,27 @@
+﻿/*
+51nod 1134 最长递增子序列
+*/
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+__int64 a[50005];
+__int64 dp[50005];
+void solve(int n)
+{
+    fill(dp,dp+50005,1e9+5);
+    for(int i=0; i<n; i++)
+        *lower_bound(dp,dp+n,a[i])=a[i];
+    cout<<lower_bound(dp,dp+n,1e9+5)-dp<<endl;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++)
+        cin>>a[i];
+    solve(n);
+    return 0;
+}
---
+++ poj/2192 Zipper.cpp
@@ -0,0 +1,36 @@
+﻿/*
+
+POJ 2192 Zipper
+
+*/
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+char a[205],b[205],c[405];
+int k[405][405];
+int main()
+{
+    int n;
+    cin>>n;
+    for(int s=1; s<=n; s++)
+    {
+        cin>>a>>b>>c;
+        int n=strlen(a),m=strlen(b);
+        memset(k,0,sizeof(k));
+        k[0][0]=1;
+        for(int i=0; i<=n; i++)
+            for(int j=0; j<=m; j++)
+            {
+                if(i>0&&c[i+j-1]==a[i-1])k[i][j]=(k[i][j]||k[i-1][j]);
+                if(j>0&&c[i+j-1]==b[j-1])k[i][j]=(k[i][j]||k[i][j-1]);
+            }
+        printf("Data set %d: ",s);
+        if(k[n][m])cout<<"yes"<<endl;
+        else cout<<"no"<<endl;
+    }
+    return 0;
+}
