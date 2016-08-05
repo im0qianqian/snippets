abe2c88186ac31e3106c4f0b0b5c6c0eb39e833d
千千 <qian1335661317@qq.com>
2016-08-05 17:22:54 +0800

    51nod

---
+++ 51nod/动态规划/51nod 1083 矩阵取数问题.cpp
@@ -0,0 +1,19 @@
+﻿/*
+51nod 1083 矩阵取数问题
+*/
+#include <iostream>
+using namespace std;
+int a[550][550],dp[550][550];
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++)
+        for(int j=1; j<=n; j++)
+            cin>>a[i][j];
+    for(int i=1; i<=n; i++)
+        for(int j=1; j<=n; j++)
+            dp[i][j]=a[i][j]+max(dp[i-1][j],dp[i][j-1]);
+    cout<<dp[n][n]<<endl;
+    return 0;
+}
---
+++ 51nod/动态规划/51nod 1183 编辑距离.cpp
@@ -0,0 +1,28 @@
+﻿/*
+51nod 1183 编辑距离  
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include <iostream>
+using namespace std;
+char a[1405],b[1405];
+int dp[1405][1405];
+int main()
+{
+    while(~scanf("%s %s",a,b))
+    {
+        memset(dp,0,sizeof(dp));
+        int n=strlen(a),m=strlen(b);
+        for(int i=0; i<=n; i++)
+            dp[i][0]=i;
+        for(int j=0; j<=m; j++)
+            dp[0][j]=j;
+        for(int i=1; i<=n; i++)
+            for(int j=1; j<=m; j++)
+                dp[i][j]=min(dp[i-1][j]+1,min(dp[i][j-1]+1,dp[i-1][j-1]+((a[i-1]==b[j-1])?0:1)));
+        cout<<dp[n][m]<<endl;
+    }
+    return 0;
+}
