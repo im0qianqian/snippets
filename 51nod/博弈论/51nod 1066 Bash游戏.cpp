6470a6bb4a655825b06994aec274fe68206c862b
千千 <qian1335661317@qq.com>
2016-08-08 18:01:56 +0800

    2015.08.08

---
+++ 51nod/博弈论/51nod 1066 Bash游戏.cpp
@@ -0,0 +1,23 @@
+﻿/*
+51nod 1066 Bash游戏
+*/
+
+#include <stdio.h>
+#include<iostream>
+#include <string.h>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    while(n--)
+    {
+        int a,b;
+        cin>>a>>b;
+        if(a%(b+1))cout<<"A"<<endl;
+        else cout<<"B"<<endl;
+    }
+    return 0;
+}
