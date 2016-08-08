bf5274f97e95ddf92bb06f527a22fdf8778c6ca4
千千 <qian1335661317@qq.com>
2016-08-08 17:14:25 +0800

    2016.08.08

---
+++ hdu/5752 Sqrt Bo.cpp
@@ -0,0 +1,34 @@
+﻿/*
+HDU 5752 Sqrt Bo
+*/
+
+#include <iostream>
+#include<algorithm>
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+using namespace std;
+
+int main()
+{
+    char c[1120];
+    while(gets(c))
+    {
+        if(strlen(c)>11||strcmp(c,"0")==0)cout<<"TAT"<<endl;
+        else
+        {
+            __int64 a;
+            sscanf(c,"%I64d",&a);
+            int i=0;
+            while(sqrt(a)!=1)
+            {
+                a=sqrt(a);
+                i++;
+                if(i>5)break;
+            }
+            if(i<=5)cout<<i<<endl;
+            else cout<<"TAT"<<endl;
+        }
+    }
+    return 0;
+}
---
+++ hdu/5753 Permutation Bo.cpp
@@ -0,0 +1,26 @@
+﻿/*
+HDU 5753 Permutation Bo
+*/
+
+#include <iostream>
+#include<algorithm>
+#include<stdio.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    while(cin>>n)
+    {
+        double s=0,a;
+        for(int i=0; i<n; i++)
+        {
+            cin>>a;
+            if(i==0||i==n-1)s+=a/2.0;
+            else s+=a/3.0;
+        }
+        if(n==1)printf("%lf\n",a);
+        else printf("%lf\n",s);
+    }
+    return 0;
+}
