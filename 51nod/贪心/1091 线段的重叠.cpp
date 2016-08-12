6491ab6a3dedc00e8bc7eb42bf86722b16114fd8
千千 <qian1335661317@qq.com>
2016-08-12 19:55:06 +0800

    2016.08.12

---
+++ 51nod/贪心/1091 线段的重叠.cpp
@@ -0,0 +1,40 @@
+﻿/*
+51nod 1091 线段的重叠
+*/
+
+#include<stdio.h>
+#include<string.h>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+struct po
+{
+    int start;
+    int end;
+} a[50005];
+bool cmp(po a,po b)
+{
+    if(a.start==b.start)return a.end>b.end;
+    return a.start<b.start;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++)
+        cin>>a[i].start>>a[i].end;
+    sort(a,a+n,cmp);
+    int res=0;
+    po s=a[0];
+    for(int i=1; i<n; i++)
+    {
+        if(a[i].end<=s.end) //线段i在线段i-1内
+            res=max(res,a[i].end-a[i].start);
+        else if(a[i].start<=s.end&&a[i].end>s.end)
+        {
+            res=max(res,s.end-a[i].start);
+            s=a[i];//选择最靠后的线段
+        }
+    }
+    cout<<res<<endl;
+}
