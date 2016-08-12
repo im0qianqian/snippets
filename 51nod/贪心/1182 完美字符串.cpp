4b6df0b6a891e69a5cd0dff9d50148034355b2c8
千千 <qian1335661317@qq.com>
2016-08-12 19:54:22 +0800

    2016.08.12

---
+++ 51nod/贪心/1182 完美字符串.cpp
@@ -0,0 +1,28 @@
+﻿/*
+51nod 1182 完美字符串
+*/
+
+#include<cmath>
+#include<iostream>
+#include<algorithm>
+#include<stdio.h>
+#include<string.h>
+using namespace std;
+int a[26];
+int main()
+{
+    char c[10005];
+    gets(c);
+    for(int i=0; i<(int)strlen(c); i++)
+    {
+        if(c[i]>='a')c[i]-=32;
+        a[c[i]-'A']++;
+    }
+    sort(a,a+26);
+    __int64 s=0;
+    for(int i=25; i>=0; i--)
+        s+=(i+1)*a[i];
+    cout<<s<<endl;
+    return 0;
+}
+
