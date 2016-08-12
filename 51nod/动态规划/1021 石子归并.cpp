52366390f79adda5be3782e4bb03cfd25f3007c5
千千 <qian1335661317@qq.com>
2016-08-12 20:54:45 +0800

    2016.08.12

---
+++ 51nod/动态规划/1021 石子归并.cpp
@@ -0,0 +1,31 @@
+﻿/*
+51nod 1021 石子归并
+*/
+
+#include<stdio.h>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+#define INF (1<<30)
+int dp[1005][1005];
+int a[1005],sum[1005];
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++)
+    {
+        cin>>a[i];
+        sum[i]=sum[i-1]+a[i];
+    }
+    for(int i=2; i<=n; i++)
+        for(int start=1; start<=n-i+1; start++)
+        {
+            int minn=INF,end=start+i-1;
+            for(int k=start; k<end; k++)
+                minn=min(minn,dp[start][k]+dp[k+1][end]+sum[end]-sum[start-1]);
+            dp[start][end]=minn;
+        }
+    cout<<dp[1][n]<<endl;
+    return 0;
+}
