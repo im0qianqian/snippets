ad9e53d115914ea8dbfd470dd871e930821e2d5d
千千 <qian1335661317@qq.com>
2016-08-15 09:28:53 +0800

    2016.08.15

---
+++ hdu/5842 Lweb and String.cpp
@@ -0,0 +1,33 @@
+﻿/*
+HDU 5842 Lweb and String
+*/
+
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<algorithm>
+using namespace std;
+char c[100005];
+int a[30];
+int main()
+{
+    int n;
+    scanf("%d%*c",&n);
+    for(int i=1; i<=n; i++)
+    {
+        gets(c);
+        int s=0;
+        memset(a,0,sizeof(a));
+        for(int j=0; j<(int)strlen(c); j++)
+        {
+            if(!a[c[j]-'a'])
+            {
+                a[c[j]-'a']=true;
+                s++;
+            }
+        }
+        printf("Case #%d: %d\n",i,s);
+    }
+    return 0;
+}
