8f847c6c1c9a70f42b7cec624f80a22f7a549d20
千千 <qian1335661317@qq.com>
2016-08-15 09:29:38 +0800

    2016.08.15

---
+++ hdu/5832 A water problem.cpp
@@ -0,0 +1,27 @@
+﻿/*
+HDU 5832 A water problem
+*/
+
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<algorithm>
+
+using namespace std;
+
+char s[11000000];
+int ans,b=73*137;
+int main()
+{
+    for(int i=1; gets(s); i++)
+    {
+        ans=0;
+        int l=strlen(s);
+        for(int j=0; j<l; j++)
+            ans=(ans*10+(s[j]-'0'))%b;
+        if(!ans)printf("Case #%d: YES\n",i);
+        else printf("Case #%d: NO\n",i);
+    }
+    return 0;
+}
