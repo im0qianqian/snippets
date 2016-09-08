e99d64c13f2254b90d22718ca550656f81547819
千千 <qian1335661317@qq.com>
2016-09-08 17:42:25 +0800

    2016.09.08

--- 51nod/图论/1445 变色DNA.cpp
+++ 51nod/图论/1445 变色DNA.cpp
@@ -1,7 +1,12 @@
 /*
 51nod 1445 变色DNA
 */
-
+/*
+到最后还是一道最短路的问题，只不过建图的时候mapp[i][j]的数值为第i行从0~j-1中Y的数量，其他点设为INF
+mapp指当前这种状态变成另一种状态所需要付出的代价！
+因为测试用例范围不大，所以写一个最简单的Floyd就可以了
+mapp[0][n-1]就代表从0变成n-1所付出的最小代价，若为INF，则无解
+*/
 #include<iostream>
 #include<stdio.h>
 #include<algorithm>
