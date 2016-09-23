c0085f82aa86b3177f8146e78f2803e0bfecd5e3
千千 <qian1335661317@qq.com>
2016-09-23 20:20:20 +0800

    2016.09.23

--- poj/2187 Beauty Contest.cpp
+++ poj/2187 Beauty Contest.cpp
@@ -3,7 +3,8 @@ POJ 2187 Beauty Contest
 
 题意：给出一些点，求平面最远点对距离的平方
 
-思想：我们知道，平面中最远点对一定是存在于这些点所围成的凸包中，那么，我们首先用Graham's Scan算法求出凸包中的所有点，然后枚举这些点就可以了
+思想：我们知道，平面中最远点对一定是存在于这些点所围成的凸包中，那么，我们首先用Graham's Scan算法求出凸包中的所有点，然后可以用O(n)的旋转卡壳来解决剩下的问题(当然暴力枚举也可以啦)。
+
 */
 #include<iostream>
 #include<algorithm>
@@ -20,24 +21,28 @@ struct point
     {
         return (x-b.x)*(x-b.x)+(y-b.y)*(y-b.y);
     }
+    point operator-(point s)
+    {
+        return {x-s.x,y-s.y};
+    }
 } ;
-point a[55000];
-struct stack
-{
-    point data[55000];
-    int top;
-} st;
-point minn;
+point a[55000],data[55000];
+
 int crossLeft(point p0,point p1,point p2)   //判读左转还是右转
 {
     return (p1.x-p0.x)*(p2.y-p0.y)-(p1.y-p0.y)*(p2.x-p0.x);
 }
-bool cmp(point p1,point p2)
+bool cmp(point p1,point p2)         //极角序
 {
     int tmp=crossLeft(a[0],p1,p2);
     if(tmp>0) return true;
     else if(tmp==0&&a[0].distance(p1)<a[0].distance(p2))return true;
     else return false;
+}
+bool cmp_x(point p1,point p2)       //坐标序
+{
+    if(p1.x!=p2.x)return p1.x<p2.x;
+    return p1.y<p2.y;
 }
 void init(int n)
 {
@@ -58,6 +63,7 @@ void init(int n)
     a[0]=p;
     sort(a+1,a+n,cmp);
 }
+
 int main()
 {
     int N;
@@ -65,18 +71,36 @@ int main()
     {
         init(N);
         for(int i=0; i<2; i++)
-            st.data[i]=a[i];            //前两个点入栈
-        st.top=1;
+            data[i]=a[i];         //前两个点入栈
+        int top=1;
         for(int i=2; i<N; i++)
         {
-            st.data[++st.top]=a[i];     //新点进栈
-            while(st.top>1&&crossLeft(st.data[st.top-2],st.data[st.top-1],st.data[st.top])<=0)  //向后遍历判断是否存在凹的区域
-                st.data[st.top-1]=st.data[st.top],st.top--;                                     //存在，出栈一个点
+            data[++top]=a[i];     //新点进栈
+            while(top>1&&crossLeft(data[top-2],data[top-1],data[top])<=0)  //向后遍历判断是否存在凹的区域
+                data[top-1]=data[top],top--;                               //存在，出栈一个点
         }
-        double maxx=0.0;
-        for(int i=0; i<=st.top; i++)
-            for(int j=i+1; j<=st.top; j++)
-                maxx=max(maxx,st.data[i].distance(st.data[j]));
-        printf("%.0f\n",maxx);
+        ++top;
+        /* 旋转卡壳部分 */
+        if(top==2)printf("%.0f\n",data[0].distance(data[1]));               //凸包退化的情况
+        else
+        {
+            double maxx=0.0;
+            int i=0,j=0;                        //某方向上的对踵点对
+            for(int k=0; k<top; k++)
+            {
+                if(!cmp_x(data[i],data[k]))i=k;
+                if(cmp_x(data[j],data[k]))j=k;
+            }
+            int si=i,sj=j;
+            while(i!=sj||j!=si)                 //旋转180°
+            {
+                maxx=max(maxx,data[i].distance(data[j]));
+                if(crossLeft({0,0},data[(i+1)%top]-data[i],data[(j+1)%top]-data[j])<0)
+                    i=(i+1)%top;
+                else j=(j+1)%top;
+            }
+            printf("%.0f\n",maxx);
+        }
+        /* END */
     }
 }
