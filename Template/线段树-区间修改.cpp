4c83d003cfca19732c2b4c2dfcef7a82713bd746
千千 <qian1335661317@qq.com>
2017-10-31 12:02:12 +0800

    init

--- Template/线段树-区间修改.cpp
+++ Template/线段树-区间修改.cpp
@@ -1,96 +1,211 @@
-﻿#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<iostream>
+﻿#include<bits/stdc++.h>
+#define IO ios::sync_with_stdio(false);\
+    cin.tie(0);\
+    cout.tie(0);
+#define default_setv -1
+
 using namespace std;
-#define MAXX 110000
-typedef long ll;
-struct po
-{
-    ll add,set,sum;
-    void init(int sum=0,int add=0,int set=-1)   //修改属性，当add为0时重置set为-1
-    {
-        this->add=add;
-        this->sum=sum;
-        if(!add)this->set=set;
-    }
-    void addmax(int sum=0,int add=0)            //属性累加
-    {
-        this->add+=add;
-        this->sum+=sum;
-    }
-} data[MAXX<<2];
+typedef long long LL;
+const int maxn = 2e5+10;
 
-int LR=1;               //满二叉树的宽度-1
-void init(int n)
+int sumv[maxn<<2];  //四倍空间
+int minv[maxn<<2];
+int maxv[maxn<<2];
+int addv[maxn<<2];
+int setv[maxn<<2];  //初始值：-1
+int a[maxn];        //输入[1,n]
+
+void maintain(int o,int L,int R)
 {
-    while(LR<n)LR<<=1;  //根据n计算满二叉树宽度
-    for(int i=0; i<=2*LR; i++)  //初始化
-        data[i].init();
-    LR--;
+    int lc = o<<1,rc=o<<1|1;
+    sumv[o] = minv[o] = maxv[o] = 0;
+    if(setv[o] != default_setv)
+    {
+        sumv[o] = setv[o] * (R-L+1);
+        minv[o] = maxv[o] = setv[o];
+    }
+    else if(R>L)
+    {
+        sumv[o] = sumv[lc] + sumv[rc];
+        minv[o] = min(minv[lc],minv[rc]);
+        maxv[o] = max(maxv[lc],maxv[rc]);
+    }
+    minv[o] += addv[o];
+    maxv[o] += addv[o];
+    sumv[o] += addv[o] * (R-L+1);
 }
 
-void inse(int o,int k)  //更新满二叉树最底层第o个节点，并自底而上更新其父节点的值
+void pushdown(int o)
 {
-    o+=LR;              //LR为满二叉树宽度-1，也是最底层第一个元素的下标
-    data[o].sum=k;
-    while(o>1)
+    int lc = o<<1,rc = o<<1|1;
+    if(setv[o] != default_setv)
     {
-        o/=2;
-        data[o].sum=data[o*2].sum+data[o*2+1].sum;
+        setv[lc] = setv[rc] = setv[o];
+        addv[lc] = addv[rc] = 0;
+        setv[o] = default_setv;
+    }
+    if(addv[o] != 0)
+    {
+        addv[lc] += addv[o];
+        addv[rc] += addv[o];
+        addv[o] = 0;
     }
 }
 
-void update(int isset,int s,int l,int r,int ls,int lr,int o)    //当前区间[l,r]，目标区间[ls,lr]，当前节点o，s为[add||set]的值
+/*
+ * o: 当前节点(1)，修改区间：[y1,y2] ，添加值：v_add，总区间：[L,R] (这里的L,R必须是2的幂次)
+ */
+void update_add(int o,int L,int R,int y1,int y2,int v_add)
 {
-    if(l>lr||r<ls||r<l)return;      //如果两个区间交集为空
-    if(r<=lr&&l>=ls)                //如果目标区间包含当前区间，执行[set||add]
+    int lc = o<<1,rc = o<<1|1;
+    if(y1<=L&&y2>=R)
+        addv[o] += v_add;
+    else
     {
-        if(isset)data[o].init(s*(r-l+1),0,s);
+        pushdown(o);
+        int M = L + (R-L)/2;
+        if(y1<=M)
+            update_add(lc,L,M,y1,y2,v_add);
         else
-        {
-            data[o].addmax(s*(r-l+1),s);
-//          data[o].init(data[o].sum+s*(r-l+1),data[o].add+s);    不能写成这样，有一个很小很小的BUG (当data[o].add+s==0时会重置set)
-        }
-        return;
+            maintain(lc,L,M);
+        if(y2>M)
+            update_add(rc,M+1,R,y1,y2,v_add);
+        else
+            maintain(rc,M+1,R);
     }
-    int m=(l+r)/2,lc=o*2,rc=lc+1;
-    if(l!=r&&(data[o].set!=-1||data[o].add!=0))     //如果不在叶子节点并且当前节点带有[set||add]属性
-    {
-        if(data[o].set!=-1)     //带有[set]属性
-        {
-            data[lc].init(data[o].set*(m-l+1),0,data[o].set);   //转移set到其两个子节点
-            data[rc].init(data[o].set*(r-m),0,data[o].set);
-            data[o].set=-1;     //重置当前节点set
-        }
-        if(data[o].add!=0)      //带有[add]属性
-        {
-            data[lc].addmax((m-l+1)*data[o].add,data[o].add);   //转移add到其两个子节点
-            data[rc].addmax((r-m)*data[o].add,data[o].add);
-            data[o].add=0;      //重置当前节点add
-        }
-    }
-    update(isset,s,l,m,ls,lr,lc);   //递归子节点
-    update(isset,s,m+1,r,ls,lr,rc);
-    data[o].sum=data[lc].sum+data[rc].sum;  //递归结束返回时计算父节点的值
+    maintain(o,L,R);
 }
-int main()
+
+/*
+ * o: 当前节点(1)，修改区间：[y1,y2] ，修改值：v_set，总区间：[L,R] (这里的L,R必须是2的幂次)
+ */
+void update_set(int o, int L, int R,int y1,int y2,int v_set)
 {
-    int k,t,n;
-    scanf("%d%d",&n,&t);
-    ++n;
-    init(n);
+    int lc = o<<1, rc = o<<1|1;
+    if(y1 <= L && y2 >= R)
+    {
+        setv[o] = v_set;
+        addv[o] = 0;
+    }
+    else
+    {
+        pushdown(o);
+        int M = L + (R-L)/2;
+        if(y1 <= M)
+            update_set(lc, L, M,y1,y2,v_set);
+        else
+            maintain(lc, L, M);
+        if(y2 > M)
+            update_set(rc, M+1, R,y1,y2,v_set);
+        else
+            maintain(rc, M+1, R);
+    }
+    maintain(o, L, R);
+}
+
+/*
+ * o: 当前节点(1)，查询区间：[y1,y2] ，初始累加(0)，总区间：[L,R] (这里的L,R必须是2的幂次)
+ */
+int _min, _max, _sum;
+void query(int o, int L, int R, int y1,int y2, int add)
+{
+    if(setv[o] != default_setv)
+    {
+        _sum += (add+setv[o]+addv[o]) * (min(R, y2)-max(L, y1)+1);
+        _min = min(_min, setv[o]+addv[o]+add);
+        _max = max(_max, setv[o]+addv[o]+add);
+    }
+    else if(y1 <= L && y2 >= R)
+    {
+        _sum += sumv[o] + add * (R-L+1);
+        _min = min(_min, minv[o]+add);
+        _max = max(_max, maxv[o]+add);
+    }
+    else
+    {
+        int M = L + (R-L)/2;
+        if(y1 <= M)
+            query(o*2, L, M, y1, y2,add+addv[o]);
+        if(y2 > M)
+            query(o*2+1, M+1, R, y1, y2, add + addv[o]);
+    }
+}
+
+/*
+ * o: 当前节点(1)，总区间：[L,R] (这里的L,R必须是2的幂次)
+ * 所有 setv 初始化为 -1，所有叶子节点 addv 为当前值
+ */
+void build(int o,int L,int R)
+{
+    if(L==R)
+    {
+        setv[o] = default_setv;
+        addv[o] = sumv[o] = maxv[o] = minv[o] = a[L];
+    }
+    else
+    {
+        int M = L + (R-L)/2;
+        int lc = o<<1,rc = o<<1|1;
+        build(lc,L,M);
+        build(rc,M+1,R);
+
+        addv[o] = 0;
+        setv[o] = default_setv;
+        sumv[o] = sumv[lc] + sumv[rc];
+        maxv[o] = max(maxv[lc],maxv[rc]);
+        minv[o] = min(minv[lc],minv[rc]);
+    }
+}
+
+/*
+ * 测试函数，R 代表叶子节点个数
+ */
+void test(int R,int n)
+{
+    cout<<"----------"<<endl;
+    auto init = [&]()
+    {
+        _sum = 0;
+        _min = INT_MAX;
+        _max = INT_MIN;
+    };
     for(int i=1; i<=n; i++)
     {
-        scanf("%d",&k);
-        inse(i,k);
+        init();
+        query(1,1,R,i,i,0);
+        cout<<i<<" --> "<<_sum<<"\t "<<_min<<"\t "<<_max<<endl;
     }
-    while(t--)
+    init();
+    query(1,1,R,1,R,0);
+    cout<<"total: "<<_sum<<endl;
+    cout<<"----------"<<endl;
+}
+
+int main()
+{
+    IO;
+    int n,m;
+    while(cin>>n>>m)
     {
-        int a,b,c;
-        scanf("%d%d%d%d",&k,&a,&b,&c);
-        update(k,c,0,LR,a,b,1);
-        printf("%ld\n",data[1].sum);
+        int R = 1;
+        while(R<n)R<<=1;        //确定叶子节点个数
+        for(int i=1; i<=n; i++)
+            cin>>a[i];
+        build(1,1,R);           //建树
+        while(m--)
+        {
+            char c;
+            int l,r;
+            cin>>c>>l>>r;
+            if(c=='Q')
+            {
+                _max = 0;
+                query(1,1,R,l,r,0);
+                cout<<_max<<endl;
+            }
+            else
+                update_set(1,1,R,l,l,r);
+        }
     }
     return 0;
 }
